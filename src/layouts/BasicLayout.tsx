1ae6732d6fec5bc433315a73beb67ab8500ba983
norlandsoft <11669556@qq.com>
2024-04-19 16:14:34 +0800

    example page

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import {Button} from 'aird';
+import ButtonPage from '@/pages/ButtonPage';
+import styles from './BasicLayout.less';
 
 const BasicLayout: React.FC = () => {
+
+  const contentHeight = window.innerHeight - 50;
+  const contentWidth = window.innerWidth - 200;
+
   return (
-    <Button type='primary'>
-      测试
-    </Button>
+    <div className={styles.container}>
+      <div className={styles.top}>AirDesign</div>
+      <div className={styles.left} style={{height: contentHeight}}>MENU</div>
+      <div className={styles.content} style={{height: contentHeight, width: contentWidth}}>
+        PAGE
+      </div>
+    </div>
   );
 };
 
---
+++ src/pages/ButtonPage.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'aird';
+
+const ButtonPage = () => {
+  return (
+    <div>
+      <Button type='primary'>
+        测试
+      </Button>
+    </div>
+  )
+}
+
+export default ButtonPage
