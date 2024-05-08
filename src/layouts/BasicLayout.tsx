7e30e1c3284b14bb5c9fe5657b289cfc9bd0b58e
norlandsoft <11669556@qq.com>
2024-05-08 16:37:50 +0800

    update

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import ButtonPage from '@/pages/ButtonPage';
 import styles from './BasicLayout.less';
 
 const BasicLayout: React.FC = () => {
-
-  const contentHeight = window.innerHeight - 50;
-  const contentWidth = window.innerWidth - 200;
-
   return (
     <div className={styles.container}>
-      <div className={styles.top}>AirDesign</div>
-      <div className={styles.left} style={{height: contentHeight}}>MENU</div>
-      <div className={styles.content} style={{height: contentHeight, width: contentWidth}}>
-        PAGE
-      </div>
+      <h1>AirDesign</h1>
     </div>
   );
 };
