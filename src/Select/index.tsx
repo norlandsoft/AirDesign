812cfd5f2dbf61a024e0e0c03e4e90bdb2dc8aa7
norlandsoft <11669556@qq.com>
2026-05-19 19:34:45 +0800

    fix(Select): 修复ConfigContext导入错误，使用ConfigProvider.ConfigContext

--- src/Select/index.tsx
+++ src/Select/index.tsx
@@ -7,23 +7,31 @@
  * - 禁用状态灰色背景
  * - 下拉面板统一风格
  *
+ * 支持默认前缀（ant-）和自定义前缀（air-，通过 ConfigProvider prefixCls 设置）。
+ *
  * @author ChaiMingXu, on 2026/05/19
  */
-import React, {forwardRef} from 'react'
-import {Select as AntSelect} from 'antd'
+import React, {forwardRef, useContext} from 'react'
+import {Select as AntSelect, ConfigProvider} from 'antd'
 import type {SelectProps as AntSelectProps, RefSelectProps} from 'antd'
 import './index.less'
 
 export type SelectProps = AntSelectProps
 
 const Select = forwardRef<RefSelectProps, SelectProps>((props, ref) => {
-  const {className = '', ...rest} = props
+  const {className = '', popupClassName = '', ...rest} = props
+  // 获取 antd 的 prefixCls，默认为 'ant'
+  const {getPrefixCls} = useContext(ConfigProvider.ConfigContext)
+  const prefixCls = getPrefixCls?.('select') ?? 'ant-select'
+
+  // 下拉面板 class：使用 antd 的 popupClassName 配合自定义 air-select-dropdown
+  const dropdownClass = `${prefixCls}-dropdown air-select-dropdown ${popupClassName}`.trim()
 
   return (
     <AntSelect
       ref={ref}
       className={`air-select ${className}`.trim()}
-      popupClassName="air-select-dropdown"
+      popupClassName={dropdownClass}
       {...rest}
     />
   )
