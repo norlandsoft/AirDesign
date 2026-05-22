999d28e82825166773fe636583a8e5afeb6695c1
norlandsoft <11669556@qq.com>
2026-05-22 11:07:05 +0800

    增强button

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,6 +1,7 @@
 /**
  * Button 毛玻璃风格按钮，支持 default/primary/danger/text/link 五种类型
  * icon 支持 string（图标名，用 Icon 渲染）或 ReactNode
+ * 兼容 Ant Design Button 的 htmlType/block/size/className 等属性
  * Created by ChaiMingXu
  */
 import React, {FC, MouseEvent, ReactNode} from 'react'
@@ -9,11 +10,15 @@ import './index.less'
 
 interface ButtonProps {
   type?: string
+  htmlType?: 'submit' | 'reset' | 'button'
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void
   style?: React.CSSProperties
   disabled?: boolean
   icon?: ReactNode | string
   loading?: boolean
+  block?: boolean
+  size?: 'small' | 'middle' | 'large'
+  className?: string
   children?: ReactNode
 
   [key: string]: unknown
@@ -24,10 +29,14 @@ const Button: FC<ButtonProps> = (props) => {
     children,
     onClick,
     type = 'default',
+    htmlType,
     style = {},
     disabled = false,
     icon = null,
     loading = false,
+    block,
+    size,
+    className,
     ...restProps
   } = props
 
@@ -37,10 +46,17 @@ const Button: FC<ButtonProps> = (props) => {
   const iconNode =
       icon == null ? null : typeof icon === 'string' ? <Icon name={icon} size={16} color={iconColor}/> : icon
 
+  // 组合 className，支持外部传入和 block 模式
+  const classList = ['air-button']
+  if (!isDisabled) classList.push(`air-button-${type}`)
+  if (className) classList.push(className)
+  if (block) classList.push('air-button-block')
+
   return (
       <button
+          type={htmlType}
           tabIndex={-1}
-          className={isDisabled ? 'air-button' : `air-button air-button-${type}`}
+          className={classList.join(' ')}
           onClick={onClick}
           style={style}
           disabled={isDisabled}
